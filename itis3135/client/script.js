7a85547cf1f188f5fabd1c8f42fa2ca27d4c695d
Alexander Prechtel <aprechtel0043@gmail.com>
2025-04-06 19:44:37 -0400

    Hopefully fixed last errors

--- itis3135/client/script.js
+++ itis3135/client/script.js
@@ -1,23 +1,30 @@
 const regex = /((?:basics|applications)\/)[^/]+\.html$/;
 const load = (location) => {
-    if (regex.test(window.location.toString())){
+    if (regex.test(window.location.toString())) {
         return window.location = window.location.toString().replace(regex, '') + location;
     }
     return window.location = location;
 };
+const learn = document.getElementById('learn');
+const resources = document.getElementById('resources');
+const variables = document.getElementById('variables');
+const types = document.getElementById('types');
+const functions = document.getElementById('functions');
+const classes = document.getElementById('classes');
+const kmp = document.getElementById('kmp');
+const android = document.getElementById('android');
 
-
-const basicLinks = {
-    variables: 'basics/variables.html',
-    types: 'basics/types.html',
-    functions: 'basics/functions.html',
-    classes: 'basics/classes.html'
-};
-const mainLinks = {
-    index: 'index.html',
-    resources: 'resources.html'
-};
-const applicationLinks = {
-  kmp: 'applications/kmp.html',
-  android: 'applications/android.html'
-};
+[
+    {element: learn, link: 'index.html'},
+    {element: resources, link: 'resources.html'},
+    {element: variables, link: 'basics/variables.html'},
+    {element: types, link: 'basics/types.html'},
+    {element: functions, link: 'basics/functions.html'},
+    {element: classes, link: 'basics/classes.html'},
+    {element: kmp, link: 'applications/kmp.html'},
+    {element: android, link: 'applications/android.html'}
+].forEach((item) => {
+    item.element.addEventListener('click', () => {
+        load(item.link);
+    });
+});
