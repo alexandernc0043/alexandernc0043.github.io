9cd2b9b6308870d5d72fd725af7914fc61a446f5
Alexander Prechtel <aprechtel0043@gmail.com>
2025-04-06 01:31:53 -0400

    Good Progress

---
+++ itis3135/client/script.js
@@ -0,0 +1,18 @@
+const load = (location) => window.location = location;
+
+const basicLinks = {
+    introduction: 'basics/index.html',
+    variables: 'basics/variables.html',
+    types: 'basics/types.html',
+    functions: 'basics/functions.html',
+    classes: 'basics/classes.html'
+};
+const mainLinks = {
+    index: 'index.html',
+    resources: 'resources.html'
+};
+const applicationLinks = {
+  introduction: 'applications/index.html',
+  kmp: 'applications/kmp.html',
+  android: 'applications/android.html'
+};
